8255da71734fbbd07421b6dcb8837656b109c8ec
KCUBE21 <kessiekoranteng88@gmail.com>
2023-08-22 13:21:30 -0700

    Return length of a string

---
+++ 0x05-pointers_arrays_strings/2-main.c
@@ -0,0 +1,18 @@
+i#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char *str;
+    int len;
+
+    str = "My first strlen!";
+    len = _strlen(str);
+    printf("%d\n", len);
+    return (0);
+}
