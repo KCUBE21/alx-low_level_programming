b8e53ed4aa27d99e501fc7c86e97881310349579
KCUBE21 <kessiekoranteng88@gmail.com>
2024-01-25 16:21:30 -0800

    All tasks

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -12,16 +12,16 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	unsigned int i, sum = 0;
+	va_list args;
+
 	if (n == 0)
 		return (0);
 
-	int sum = 0;
-
-	va_list args;
 
 	va_start(args, n);
 
-	for (int i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 		sum += va_arg(args, int);
 
 	va_end(args);
