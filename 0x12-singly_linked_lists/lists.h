81ff459c0a47f66f3dc10405d4e8b43cea4592d6
KCUBE21 <kessiekoranteng88@gmail.com>
2024-02-01 15:12:51 -0800

    All tasks

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,6 +6,5 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-sigset_t *add_node(list_t **head, const char *str);
 
 #endif
