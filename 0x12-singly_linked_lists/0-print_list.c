c4ea1e80ced2cd12e4bbbd1af2af74807278245e
KCUBE21 <kessiekoranteng88@gmail.com>
2024-02-01 15:45:42 -0800

    All tasks

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
 #include "lists.h"
 
@@ -11,21 +9,17 @@
 */
 size_t print_list(const list_t *h)
 {
-   int count = 0;
+	size_t s = 0;
 
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h= h->next;
+		s++;
+	}
 
-   while (h)
-   {
-       if (h->str == NULL)
-       {
-           printf("[0] (nil)\n");
-       }
-       else
-       {
-       printf("[%d] %s\n", h->len, h->str);
-       }
-       count++;
-       h = h->next;
-   }
-   return (count);
+	return (s);
 }
